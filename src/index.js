33cbea8fdc21b421f1cd47ecea5c8862f6ec182f
dannyseo517 <dannyseo517@gmail.com>
2021-08-09 11:38:45 -0700

    add express and body-parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-var http = require("http");
+// requires
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('GET REQUEST');
+});
+
+const listener = app.listen(8080, () => {
+  console.log('listening on port: ' + listener.address().port)
+})
